846b375194f30b869ac38373fc5b9fc7fe08f9f0
royalturd <samridhh.rudra@hotmail.com>
2020-10-18 03:57:47 +0530

    introduction
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

---
+++ intro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printf "\n\tHELLO I AM.....\n\n"
+
+sleep 3s
+    printf "\t.--------------------------------------------.\n"
+    printf "\t|                               (   )        |\n"
+    printf "\t|                             (   ) (        |\n"
+    printf "\t|   █▀█ █▀█ █▄█ ▄▀█ █░░       ) _   )|       |\n"
+    printf "\t|   █▀▄ █▄█ ░█░ █▀█ █▄▄        ( \_          |\n"
+    printf "\t|                            _(_\ \)__       |\n"
+    printf "\t|                           (____\___))      |\n" 
+    printf "\t|                                            | \n"
+    printf "\t|             royalturd                      | \n"
+    printf "\t|                                            | \n"
+    printf "\t'--------------------------------------------'\n"
+
